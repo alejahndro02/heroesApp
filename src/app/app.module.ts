f1330af7a27ac6db81ed3eb8d7bd2356a7ac96aa
alejahndro02 <alejahndro02@gmail.com>
2022-02-04 13:13:34 -0600

    Se modifico la ruta del componente errorPageomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule           } from '@angular/platform-browser';
 import { AppRoutingModule        } from './app-routing.module';
 import { AppComponent            } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ErrorPageComponent      } from './shared/error-page/error-page.component';
+import { ErrorPageComponent      } from '@error/error-page.component';
 
 @NgModule({
   declarations: [
