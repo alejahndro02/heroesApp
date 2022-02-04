30f553c8718358218b6ce722561b20aa1b11fc4c
alejahndro02 <alejahndro02@gmail.com>
2022-02-04 12:48:55 -0600

    Se agregaron sangrias a las importaciones

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AgregarComponent } from './pages/agregar/agregar.component';
-import { BuscarComponent } from './pages/buscar/buscar.component';
-import { HeroeComponent } from './pages/heroe/heroe.component';
-import { HeroesHomeComponent } from './pages/heroes-home/heroes-home.component';
-import { ListadoComponent } from './pages/listado/listado.component';
-
-
+import { NgModule            } from '@angular/core';
+import { CommonModule        } from '@angular/common';
+import { AgregarComponent    } from '@agregar/agregar.component';
+import { BuscarComponent     } from '@buscar/buscar.component';
+import { HeroeComponent      } from '@heroe/heroe.component';
+import { HeroesHomeComponent } from '@heroes-home/heroes-home.component';
+import { ListadoComponent    } from '@listado/listado.component';
 
 @NgModule({
   declarations: [
