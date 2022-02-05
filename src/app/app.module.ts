f1dc352b3b9a9203edcf2d4b08cd1f621850a43b
alejahndro02 <alejahndro02@gmail.com>
2022-02-05 11:56:21 -0600

    Se importo el modulo de httpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule                } from '@angular/core';
 import { BrowserModule           } from '@angular/platform-browser';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule        } from '@angular/common/http';
       
 import { AppRoutingModule        } from './app-routing.module';
 import { AppComponent            } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorPageComponent      } from '@error/error-page.component';
 
 @NgModule({
@@ -13,8 +15,9 @@ import { ErrorPageComponent      } from '@error/error-page.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
