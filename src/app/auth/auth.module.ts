d6051a2738bb7971e85b826007b8b87a177dbc93
alejahndro02 <alejahndro02@gmail.com>
2022-02-04 18:30:31 -0600

    Se imiporta el archivo de authRoutingModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { NgModule          } from '@angular/core';
 import { CommonModule      } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent    } from '@login/login.component';
 import { RegistroComponent } from '@registro/registro.component';
-
-
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { RegistroComponent } from '@registro/registro.component';
     RegistroComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
