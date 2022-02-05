ae696b5389b4d61e6660f016f5e606b08792ed8f
alejahndro02 <alejahndro02@gmail.com>
2022-02-04 19:11:45 -0600

    Se creo el modulo de heroesRouting y se declararon las rutas de los componentes a renderizar en el html

---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,52 @@
+import { NgModule             } from '@angular/core';
+import { CommonModule         } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AgregarComponent     } from '@agregar/agregar.component';
+import { BuscarComponent      } from '@buscar/buscar.component';
+import { HeroeComponent       } from '@heroe/heroe.component';
+import { ListadoComponent     } from '@listado/listado.component';
+
+const routes : Routes =[
+  {
+    path:'',
+    children:[
+      {
+        path:'listado',
+        component: ListadoComponent
+      },
+      {
+        path:'agregar',
+        component: AgregarComponent
+      },
+      {
+        path:'editar/:id',
+        component: AgregarComponent
+      },
+      {
+        path:'buscar',
+        component: BuscarComponent
+      },
+      {
+        path:':id',
+        component: HeroeComponent
+      },
+      {
+        path:'**',
+        redirectTo:'listado'
+      },
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class HeroesRoutingModule { }
