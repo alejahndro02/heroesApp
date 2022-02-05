3d7dda053b2780cebec9f7de2045e30272489055
alejahndro02 <alejahndro02@gmail.com>
2022-02-04 19:17:56 -0600

    Se modifgicaron las rutas de los componentes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule             } from '@angular/core';
 import { CommonModule         } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent       } from './pages/login/login.component';
-import { RegistroComponent    } from './pages/registro/registro.component';
+import { LoginComponent       } from '@login/login.component';
+import { RegistroComponent    } from '@registro/registro.component';
 
 const routes : Routes = [
   {
