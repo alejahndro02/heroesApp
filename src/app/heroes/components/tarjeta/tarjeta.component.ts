75421e629e38232f69d7eb124b6f243a8a358ace
alejahndro02 <alejahndro02@gmail.com>
2022-02-07 10:32:34 -0600

    Se paso la informacion a travez del decorador input, se maqueto la vista del componente utilizando mat card

---
+++ src/app/heroes/components/tarjeta/tarjeta.component.ts
@@ -0,0 +1,22 @@
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Heroe } from '@heroes/interface/heroes.interface';
+import { HeroesService } from '@heroes/services/heroes.service';
+
+@Component({
+  selector: 'app-tarjeta',
+  templateUrl: './tarjeta.component.html',
+  styles: [
+    `mat-card{
+      margin-top:20px
+    }`
+  ]
+})
+export class TarjetaComponent implements OnInit {
+  @Input() superHeroe! : Heroe
+  constructor(private heroesService: HeroesService) { }
+
+  ngOnInit(): void {
+  }
+
+}
