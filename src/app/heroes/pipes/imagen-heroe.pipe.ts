002ef026049d52d0b19f992d6ccfa0fa172c7294
alejahndro02 <alejahndro02@gmail.com>
2022-02-07 18:23:52 -0600

    Al metodo transform se le pasa como argumento la interface de Hero y este retorna la ruta de la img

--- src/app/heroes/pipes/imagen-heroe.pipe.ts
+++ src/app/heroes/pipes/imagen-heroe.pipe.ts
@@ -1,12 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Heroe               } from "@interface/heroes.interface"
 
 @Pipe({
   name: 'imagenHeroe'
 })
 export class ImagenHeroePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(supeHeroeImg: Heroe ): string{
+    return `assets/heroes/${supeHeroeImg.id}.jpg`
   }
 
 }
