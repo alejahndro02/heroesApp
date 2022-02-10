f1dc72a5231c5c02b6b166b9b160f4ea37455daf
alejahndro02 <alejahndro02@gmail.com>
2022-02-10 11:49:02 -0600

    se creo un metodo para obtener los datos del hereo seleccionado

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -14,4 +14,7 @@ export class HeroesService {
   getHeroes():Observable<Heroe[]>{
     return this.http.get<Heroe[]>('http://localhost:3000/heroes')
   }
+  getHeroesId( id:string ):Observable<Heroe>{
+    return this.http.get<Heroe>(`http://localhost:3000/heroes/${ id }`)
+  }
 }
