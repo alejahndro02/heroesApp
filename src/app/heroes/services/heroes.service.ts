b9f2506be2da2d94b4bf953014793669470fc51e
alejahndro02 <alejahndro02@gmail.com>
2022-02-10 13:21:25 -0600

    Se declaro un variable de entorno que contiene la url de la base de datos

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Heroe      } from '@interface/heroes.interface';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
+  private dataUrl: string = environment.dataUrl
 
   constructor(private http: HttpClient) { }
 
   getHeroes():Observable<Heroe[]>{
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes')
+    return this.http.get<Heroe[]>(`${this.dataUrl}/heroes`)
   }
   getHeroesId( id:string ):Observable<Heroe>{
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${ id }`)
+    return this.http.get<Heroe>(`${this.dataUrl}/heroes/${ id }`)
   }
 }
