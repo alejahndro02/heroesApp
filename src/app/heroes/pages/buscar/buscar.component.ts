45f47007a0db7926e31c130e58637dbefb9b1a5c
alejahndro02 <alejahndro02@gmail.com>
2022-02-10 23:17:02 -0600

    Se declaro el metodo opcionSeleccionada y se le pasa como argumento un evento de tipo matAutoCompleteSelectedEvent

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Heroe } from '@heroes/interface/heroes.interface';
-import { HeroesService } from '@heroes/services/heroes.service';
+import { Component, OnInit            } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Heroe                        } from '@heroes/interface/heroes.interface';
+import { HeroesService                } from '@heroes/services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
@@ -16,7 +17,13 @@ export class BuscarComponent implements OnInit {
   ngOnInit(): void {
   }
   buscando(){
-    this.heroesService.getHeroes().subscribe(heroes=> this.superHeroes = heroes)
+    // this.heroesService.getHeroes().subscribe(heroes=> this.superHeroes = heroes)
+    this.heroesService.getSugerencias(this.termino).subscribe(heroes=> this.superHeroes = heroes)
+
+  }
+  opcionSeleccionada(event:MatAutocompleteSelectedEvent){
+    console.log(event);
+    
   }
 
 }
