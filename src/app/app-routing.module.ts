ccb8f231e47e37cd17abe683cc707bb17ab19507
alejahndro02 <alejahndro02@gmail.com>
2022-02-22 15:58:02 -0600

    se agrego la propiedad canLoad y como valor el nombre del guard en este caso AuthGuard dentro de la ruta que se desea proteger

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule             } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorPageComponent } from '@error/error-page.component';
+import { AuthGuard            } from '@auth/guards/auth.guard';
+import { ErrorPageComponent   } from '@error/error-page.component';
 
 const routes: Routes = [
   {
@@ -9,7 +10,8 @@ const routes: Routes = [
   },
   {
     path:'heroes',
-    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule )
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule ),
+    canLoad:[AuthGuard]
   },
   {
     path:'404', 
