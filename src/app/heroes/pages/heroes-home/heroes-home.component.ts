a2c1abd493a2189839846981e3e0da03434cd574
alejahndro02 <alejahndro02@gmail.com>
2022-02-22 12:47:06 -0600

    Se importo el servico de authService

--- src/app/heroes/pages/heroes-home/heroes-home.component.ts
+++ src/app/heroes/pages/heroes-home/heroes-home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router            } from '@angular/router';
+
+import { Auth              } from '@auth/intreface/auth.interface.';
+import { AuthService       } from '@auth/service/auth.service';
 
 @Component({
   selector: 'app-heroes-home',
@@ -11,12 +14,18 @@ import { Router } from '@angular/router';
   ]
 })
 export class HeroesHomeComponent implements OnInit {
-
-  constructor(private router:Router) { }
+  get auth(){
+    return this.authService.auth
+  }
+  constructor(private router:Router,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.auth.id
   }
   logOut(){
+
     this.router.navigate(['auth'])
   }
+
 }
