31236a7f79f044159d6a47ff8612a9b9379ab459
alejahndro02 <alejahndro02@gmail.com>
2022-02-22 11:46:26 -0600

    Se importo y se inyecto el authService y se ejcuta dentro del metodo Login llamando almetodologIn

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router            } from '@angular/router';
+
+import { AuthService       } from '@auth/service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: []
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor( private router:Router,
+               private authService:AuthService) { }
 
   ngOnInit(): void {
   }
  logIn(){
-  this.router.navigate(['/heroes'])
+   this.authService.logIn().subscribe(auth =>{
+     if(auth.id){
+       this.router.navigate(['/heroes'])
+     }
+   })
  }
 }
