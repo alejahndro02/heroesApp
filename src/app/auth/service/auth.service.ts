d1b2084535e1235c402ec8c6a55d15a87027d9cf
alejahndro02 <alejahndro02@gmail.com>
2022-02-22 12:41:50 -0600

    se mporta el operador tap, se crea un getter auth

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient   } from '@angular/common/http';
 import { Injectable   } from '@angular/core';
+import { HttpClient   } from '@angular/common/http';
+import { tap          } from 'rxjs';
+
 import { environment  } from 'src/environments/environment';
 import { Auth         } from '@auth/intreface/auth.interface.';
 
@@ -7,12 +9,20 @@ import { Auth         } from '@auth/intreface/auth.interface.';
   providedIn: 'root'
 })
 export class AuthService {
+  
   private dataUrl: string = environment.dataUrl;
-
+  private _auth: Auth | undefined;
+  
+  get auth(){
+    return {...this._auth}
+  }
   constructor(private http:HttpClient) { }
   
   logIn(){
-    return this.http.get<Auth>(`${this.dataUrl}/usuarios/1`);
+    return this.http.get<Auth>(`${this.dataUrl}/usuarios/1`)
+    .pipe(
+      tap( auth => this._auth = auth)
+    )
   }
 
 }
