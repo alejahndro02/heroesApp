840c3f69cf79e94d43f168f232d796fb5ed23b3f
alejahndro02 <alejahndro02@gmail.com>
2022-02-22 15:56:31 -0600

    Se evaluo por medio de una condicional if si el authService.auth.id existe

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -6,20 +6,27 @@ import { ActivatedRouteSnapshot,
          RouterStateSnapshot, 
          UrlSegment, 
          UrlTree } from '@angular/router';
+import { AuthService } from '@auth/service/auth.service';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate, CanLoad {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
+export class AuthGuard implements  CanLoad {
+  constructor(private authService: AuthService){}
+  // canActivate(
+  //   route: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //   return true;
+  // }
   canLoad(
     route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+      
+      if(this.authService.auth.id){
+        return true;
+      }
+      console.log('Bloqueado por el AuthGuard');
+      return false;
   }
 }
