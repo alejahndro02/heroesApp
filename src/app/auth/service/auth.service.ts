5158cdb727407dc2dc8558c382d6bc0aba5cf70a
alejahndro02 <alejahndro02@gmail.com>
2022-02-22 18:58:26 -0600

    Se decalro otro pipe tap para dejar de forma persistente dentro del local storage

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -21,7 +21,9 @@ export class AuthService {
   logIn(){
     return this.http.get<Auth>(`${this.dataUrl}/usuarios/1`)
     .pipe(
-      tap( auth => this._auth = auth)
+      tap( auth => this._auth = auth),
+      tap( auth => localStorage.setItem('id',auth.id))
+
     )
   }
   logOut(){
