ebf4c5086ee7a8611a6aaffa95dd1d7c6d2377f8
alejahndro02 <alejahndro02@gmail.com>
2022-02-24 11:46:16 -0600

    se importo el modulo de router para poder hacer la redireccion en caso de que no haya untoken dentro del local storege

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,36 +1,44 @@
-import { Injectable } from '@angular/core';
+import { Injectable      } from '@angular/core';
 import { ActivatedRouteSnapshot, 
          CanActivate, 
          CanLoad, 
          Route, 
+         Router, 
          RouterStateSnapshot, 
-         UrlSegment, 
-         UrlTree } from '@angular/router';
-import { AuthService } from '@auth/service/auth.service';
-import { Observable } from 'rxjs';
+         UrlSegment,     } from '@angular/router';
+import { AuthService     } from '@auth/service/auth.service';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements  CanLoad, CanActivate {
-  constructor(private authService: AuthService){}
+  constructor(  private authService: AuthService,
+                private router: Router){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      if(this.authService.auth.id){
-        return true;
-      }
-      console.log('Bloqueado por el AuthGuard -CanActivate');
-      return false
+
+      return this.authService.verificacionAutenticacion()
+             .pipe(
+               tap(estaAutenticado =>{
+                 if(!estaAutenticado){
+                   this.router.navigate(['./auth/login'])
+                 }
+               })
+             )
   }
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-      
-      if(this.authService.auth.id){
-        return true;
-      }
-      console.log('Bloqueado por el AuthGuard-CanLoad');
-      return false;
+        
+        return this.authService.verificacionAutenticacion()
+               .pipe(
+                 tap(estaAutenticado =>{
+                   if(!estaAutenticado){
+                     this.router.navigate(['./auth/login'])
+                   }
+                 })
+               )
   }
 }
