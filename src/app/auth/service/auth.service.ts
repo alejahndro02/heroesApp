d4ebdbff3d0eb5e234553d609473bc2ced6b99db
alejahndro02 <alejahndro02@gmail.com>
2022-02-24 11:45:16 -0600

    Se declaro el metodo de verificacion Autenticacion que verificara si en el localstorage hay un token

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable   } from '@angular/core';
 import { HttpClient   } from '@angular/common/http';
-import { tap          } from 'rxjs';
-
+import { Observable, 
+         of           } from 'rxjs';
+import { map, tap     } from 'rxjs/operators'
 import { environment  } from 'src/environments/environment';
 import { Auth         } from '@auth/intreface/auth.interface.';
 
@@ -17,13 +18,26 @@ export class AuthService {
     return {...this._auth}
   }
   constructor(private http:HttpClient) { }
-  
+
+  verificacionAutenticacion():Observable<boolean>{
+    
+    if(!localStorage.getItem('token')){
+      return of (false) 
+    }
+    return this.http.get<Auth>(`${this.dataUrl}/usuarios/1`)
+           .pipe(
+             map( auth => {
+               this._auth = auth;
+               console.log(map);
+               return true;
+             })
+           )
+  }
   logIn(){
     return this.http.get<Auth>(`${this.dataUrl}/usuarios/1`)
     .pipe(
       tap( auth => this._auth = auth),
-      tap( auth => localStorage.setItem('id',auth.id))
-
+      tap( auth => localStorage.setItem('token',auth.id))
     )
   }
   logOut(){
